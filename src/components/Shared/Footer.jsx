69a73ed6617b0badb8d680091dd9416e579dbe5a
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-18 20:42:31 +0600

    footer added

---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaFacebookF, FaWhatsapp, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer className="footer items-center p-3 bg-purple-100 text-black rounded-xl fixed bottom-0">
+            <div className="flex items-center">
+                <img src="https://i.ibb.co/hsMfBj2/1170130-ORVZE20-removebg-preview.png" alt="" className='w-20 h-20' />
+                <h1 className='text-3xl font-bold'>Playtime<span className='text-purple-600'>-Paradise</span></h1>
+            </div>
+                <p>Copyright © 2023/ All rights reserved</p>
+            <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <button className='btn btn-primary'><FaFacebookF className="h-6 w-6" /></button>
+            <button className='btn btn-primary'><FaWhatsapp className="h-6 w-6" /></button>
+            <button className='btn btn-primary'><FaTwitter className="h-6 w-6" /></button>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
