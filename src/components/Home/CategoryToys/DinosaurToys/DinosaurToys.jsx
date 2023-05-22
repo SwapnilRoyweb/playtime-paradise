23b94723d5d3514237188dca3679ea52d1fc18b8
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-22 13:03:09 +0600

    some problem fixed

--- src/components/Home/CategoryToys/DinosaurToys/DinosaurToys.jsx
+++ src/components/Home/CategoryToys/DinosaurToys/DinosaurToys.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Rating } from '@smastrom/react-rating';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../../Providers/AuthProvider';
+import Swal from 'sweetalert2';
 
 const DinosaurToys = ({dinosaurToy}) => {
 
     const { sellerName, subCategory, price, rating, quantity, description, toyPicture, toyName, _id } = dinosaurToy;
+
+    const {user} = useContext(AuthContext);
+
+    const handleDetails = () => {
+        if(!user){
+            Swal.fire(
+                'Sorry!',
+                'You have to Login or Sign up First',
+                'error'
+            )
+        }
+    }
 
     return (
         <div className="card w-96 bg-purple-300 shadow-xl">
@@ -13,9 +27,9 @@ const DinosaurToys = ({dinosaurToy}) => {
                 <h2 className="card-title">{toyName}</h2>
                 <p>{description}</p>
                 <p>Price: ${price}</p>
-                <p>Ratings: <Rating style={{ maxWidth: 150 }} value={5} onChange={rating} /></p>
+                Ratings: <Rating style={{ maxWidth: 150 }} value={5} onChange={rating} />
                 <div className="card-actions justify-end">
-                    <Link to={`/singleToy/${_id}`}><button className="btn btn-primary">View Details</button></Link>
+                    <Link to={`/singleToy/${_id}`}><button className="btn btn-primary" onClick={handleDetails}>View Details</button></Link>
                 </div>
             </div>
         </div>
